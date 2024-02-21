805ac4d39985088301dae45cff32fbf039b34d60
Patty Hung <pattyhung72@gmail.com>
2024-02-21 16:36:28 +1100

    ui: add navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+export default function Navbar() {
+  return (
+    <nav className="justify-center py-2 flex w-full select-none pt-6 font-light md:px-28 md:pb-2">
+      <div className="container flex flex-col items-center justify-between md:flex-row">
+        <div>Patty Hung</div>
+        <div className="n flex gap-x-8 text-xs md:text-base">
+          <span>About</span>
+          <span>Skills</span>
+          <span>Contact</span>
+        </div>
+      </div>
+    </nav>
+  )
+}
