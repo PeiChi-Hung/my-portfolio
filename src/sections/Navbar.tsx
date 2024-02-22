aa5cdbb5657e62ce6615fa894c1cf0870c6ddf77
Patty Hung <pattyhung72@gmail.com>
2024-02-23 00:15:26 +1100

    feat: add scroll into view in navbar

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -1,14 +1,25 @@
 export default function Navbar() {
+  const links = ["about", "skills", "contact"]
   return (
     <nav className="justify-center py-2 flex w-full select-none pt-6 font-light md:px-28 md:pb-2">
       <div className="container flex flex-col items-center justify-between md:flex-row">
         <div className="text-5xl drop-shadow-2xl font-[goldensignature]">
           Patty Hung
         </div>
-        <div className="n flex gap-x-8 text-xs md:text-base">
-          <span>About</span>
-          <span>Skills</span>
-          <span>Contact</span>
+        <div className="nav-links flex gap-x-8 text-xs md:text-base">
+          {links.map((link) => (
+            <span
+              key={link}
+              className="cursor-pointer"
+              onClick={() => {
+                document
+                  .getElementById(link)
+                  ?.scrollIntoView({ behavior: "smooth" })
+              }}
+            >
+              {link.charAt(0).toUpperCase() + link.slice(1)}
+            </span>
+          ))}
         </div>
       </div>
     </nav>
