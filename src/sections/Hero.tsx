d5408c1cc4f83a02358c0c758c5145854e0fbac4
Patty Hung <pattyhung72@gmail.com>
2024-02-24 17:41:47 +1100

    ui: update layout

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -3,15 +3,19 @@ import ContactList from "@/components/ContactList"
 export default function Hero() {
   return (
     <section className="my-8 flex flex-col items-center justify-center">
-      <img
-        src="/avatar.jpg"
-        alt="avatar"
-        className="w-[150px] h-[150px] object-center object-cover rounded-full animate-bounce-low"
-      />
+      <div className="p-3 md:p-4">
+        <img
+          src="/avatar.jpg"
+          alt="avatar"
+          className="w-[150px] h-[150px] object-center object-cover rounded-full animate-bounce-low"
+        />
+      </div>
 
       <h1 className="mb-4 text-[1.4rem] md:text-[2rem]">Hi, I'm Patty Hung</h1>
       <h1>An Aspiring Software Developer</h1>
-      <h1>Welcome to my page!</h1>
+      <div className="my-12 flex w-full flex-col gap-2 text-center lg:w-[50%]">
+        <h2>Welcome to my page!</h2>
+      </div>
       <ContactList />
     </section>
   )
