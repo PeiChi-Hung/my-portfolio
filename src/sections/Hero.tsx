6989c8fc51f091e975dfa880ff5980eb3f6efa9f
Patty Hung <pattyhung72@gmail.com>
2024-12-14 00:58:19 +1100

    feat: add responsiveness for mobile

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,26 +1,22 @@
-// import { AvatarSitting } from "@/components/3d/AvatarSitting"
 import { AvatarWaving } from "@/components/3d/AvatarWaving"
 import ContactList from "@/components/ContactList"
 import { motion } from "motion/react"
 
 export default function Hero() {
   return (
-    <section
-      className="hero h-full 
-     flex relative lg:p-0"
-    >
+    <section className="hero-mobile flex relative lg:hero lg:h-full lg:p-0">
       <div className="w-full">
         <AvatarWaving />
       </div>
 
-      <motion.div
-        className="absolute flex flex-col lg:top-1/3 gap-4"
-        animate={{ x: 100, transition: { duration: 2 } }}
+      <div
+        className="flex flex-col absolute text-center items-center gap-4 m-10 mx-auto  lg:top-1/3 "
+        // animate={{ x: 100, transition: { duration: 2 } }}
       >
         <h1 className="text-[1.4rem] md:text-[2rem]">Hi, I'm Patty Hung</h1>
         <h1 className="xl:w-3/4 2xl:w-full">An Aspiring Software Developer</h1>
         <ContactList />
-      </motion.div>
+      </div>
     </section>
   )
 }
