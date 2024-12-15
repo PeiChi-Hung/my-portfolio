102c75aa105dbf6232f5490c5d2d02c933e087f3
Patty Hung <pattyhung72@gmail.com>
2024-12-16 01:17:04 +1100

    fix: make text center horizontally in the hero section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,22 +1,26 @@
-import { AvatarWaving } from "@/components/3d/AvatarWaving"
-import ContactList from "@/components/ContactList"
-import { motion } from "motion/react"
+import { AvatarWaving } from "@/components/3d/AvatarWaving";
+import ContactList from "@/components/ContactList";
+import { motion } from "motion/react";
 
 export default function Hero() {
   return (
-    <section className="hero-mobile flex relative lg:hero lg:h-full lg:p-0">
+    <section className="hero-mobile lg:hero relative flex lg:h-full lg:p-0">
       <div className="w-full">
         <AvatarWaving />
       </div>
 
-      <div
-        className="flex flex-col absolute text-center items-center gap-4 m-10 mx-auto  lg:top-1/3 "
-        // animate={{ x: 100, transition: { duration: 2 } }}
-      >
-        <h1 className="text-[1.4rem] md:text-[2rem]">Hi, I'm Patty Hung</h1>
-        <h1 className="xl:w-3/4 2xl:w-full">An Aspiring Software Developer</h1>
-        <ContactList />
+      <div className="absolute inset-x-0 top-0 flex items-center justify-center">
+        <div
+          className="mx-auto flex flex-col items-center gap-4 border border-blue-500 text-center lg:items-start"
+          // animate={{ x: 100, transition: { duration: 2 } }}
+        >
+          <h1 className="text-[1.4rem] md:text-[2rem]">Hi, I'm Patty Hung</h1>
+          <h1 className="xl:w-3/4 2xl:w-full">
+            An Aspiring Software Developer
+          </h1>
+          <ContactList />
+        </div>
       </div>
     </section>
-  )
+  );
 }
