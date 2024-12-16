c9d5619ae6289e42901865fbd0d595c1a7b0cc84
Patty Hung <pattyhung72@gmail.com>
2024-12-17 01:58:33 +1100

    fix: add avatar back to the scene

--- src/components/3d/AvatarWaving.tsx
+++ src/components/3d/AvatarWaving.tsx
@@ -1,11 +1,11 @@
-import { PerspectiveCamera } from "@react-three/drei"
-import { Canvas } from "@react-three/fiber"
-import { Avatar } from "./Avatar"
-import { Room } from "./Room"
-import { Leva, useControls } from "leva"
-import { useMediaQuery } from "react-responsive"
-import HeroCamera from "./HeroCamera"
-import { calculateSizes } from "@/constants"
+import { PerspectiveCamera } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Avatar } from "./Avatar";
+import { Room } from "./Room";
+import { Leva, useControls } from "leva";
+import { useMediaQuery } from "react-responsive";
+import HeroCamera from "./HeroCamera";
+import { calculateSizes } from "@/constants";
 
 export const AvatarWaving = () => {
   // const controls = useControls("Room", {
@@ -16,16 +16,16 @@ export const AvatarWaving = () => {
   //   positionX: { value: -5.1, min: -10, max: 10 },
   //   positionY: { value: -1.9, min: -10, max: 30 },
   //   positionZ: { value: 22.5, min: -10, max: 30 },
-  // })
+  // });
 
   // const isSmall = useMediaQuery({ maxWidth: 440 })
-  const isMobile = useMediaQuery({ maxWidth: 768 })
-  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 })
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 });
 
-  const sizes = calculateSizes(isMobile, isTablet)
+  const sizes = calculateSizes(isMobile, isTablet);
 
   return (
-    <div className="w-full h-full">
+    <div className="h-full w-full">
       {/* <Leva /> */}
       <Canvas className="w-full">
         <PerspectiveCamera makeDefault position={[-7, 3.7, 24.7]} />I
@@ -36,24 +36,14 @@ export const AvatarWaving = () => {
             rotation={sizes.deskRotation}
           />
         </HeroCamera>
-        {/* <Avatar
-          scale={4}
-          rotation={[0.1, -0.3, 0]}
-          position={[-4.9, 1.7, 16.5]}
-          // position={[
-          //   controls.positionX,
-          //   controls.positionY,
-          //   controls.positionZ,
-          // ]}
-          // rotation={[
-          //   controls.rotationX,
-          //   controls.rotationY,
-          //   controls.rotationZ,
-          // ]}
-        /> */}
+        <Avatar
+          scale={sizes.avatarScale}
+          rotation={sizes.avatarRotation}
+          position={sizes.avatarPosition}
+        />
         <ambientLight intensity={3} />
         <directionalLight position={[10, 10, 10]} />
       </Canvas>
     </div>
-  )
-}
+  );
+};
--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,17 +1,29 @@
-import * as THREE from "three"
+import * as THREE from "three";
 
 export const calculateSizes = (isMobile: boolean, isTablet: boolean) => {
   return {
+    // index for desk
     deskScale: isMobile ? 1.2 : isTablet ? 1.25 : 3,
     deskPosition: isMobile
       ? new THREE.Vector3(-7.3, -5.5, 0.5)
       : isTablet
-      ? new THREE.Vector3(-6.3, -3.9, 3.3)
-      : new THREE.Vector3(-2.7, -0.7, 0.5),
+        ? new THREE.Vector3(-6.3, -3.9, 3.3)
+        : new THREE.Vector3(-2.7, -0.7, 0.5),
     deskRotation: isMobile
       ? new THREE.Euler(0.1, 0.3, 0)
       : isTablet
-      ? new THREE.Euler(0.1, 0.3, 0)
-      : new THREE.Euler(0.2, 0.3, 0),
-  }
-}
+        ? new THREE.Euler(0.1, 0.3, 0)
+        : new THREE.Euler(0.2, 0.3, 0),
+
+    // index for avatar
+    avatarScale: isMobile ? 2.8 : isTablet ? 3 : 4,
+    avatarPosition: isMobile
+      ? new THREE.Vector3(-7.1, 0.1, 16.1)
+      : isTablet
+        ? new THREE.Vector3(-5.7, 0.1, 14.9)
+        : new THREE.Vector3(-4.9, 1.7, 16.5),
+    avatarRotation: isMobile
+      ? new THREE.Euler(0.1, 0, 0)
+      : new THREE.Euler(0.1, -0.3, 0),
+  };
+};
